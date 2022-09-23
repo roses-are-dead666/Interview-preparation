530844a4136634020d84c6e2dd7b8b8406325140
arzhan <chalisham666@gmail.com>
2022-09-23 20:26:19 +0600

    changed js

--- js.js
+++ js.js
@@ -9,31 +9,34 @@ function openPage(pageName) {
 
     document.getElementById("defaultOpen").click();
 
-    const menuWrap = document.querySelectorAll('.tabLink');
-
-    for (var i = 0; i < menuWrap.length; i++) {
-        menuWrap[i].addEventListener('click', select, false);
-    }
-    
-    function select(){
-      for (var i = 0; i < menuWrap.length; i++) {
-          menuWrap[i].removeAttribute('selected');
-      }
-      this.setAttribute("selected", "true"); 
-    }
-
-    function hamburgerMenu() {
-        let mobileNav = document.getElementById("mobileTabLink");
-        
-            if (mobileNav.style.display === "block") {
-                mobileNav.style.display = "none";
-            } else {
-                mobileNav.style.display = "block";
-            }
-        }
-
 $(function() {
+
+    let header = $("#header");
+    let scrollOffset = $(window).scrollTop();
+
+    checkScroll(scrollOffset);
+
     $(window).on("scroll", function() {
-        
+        scrollOffset = $(this).scrollTop();
+        checkScroll(scrollOffset);
     })
+
+    function checkScroll(scrollOffset) {
+
+        if (scrollOffset >= 200) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+    $("#navBurger").on("click", function(event) {
+        event.preventDefault();
+        $(this).toggleClass("active");
+        $("nav").toggleClass("active");
+        $( "nav a" ).on("click", function(){
+            $("#navBurger").click();
+          });
+    })
+
 })
